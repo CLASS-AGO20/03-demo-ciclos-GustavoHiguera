aea7b819b2aab642d61b8fca4ad0aa5640c961c3
Gustavo Higuera <ghiguera@ucol.mx>
2020-12-08 16:51:13 -0600

    validated range values

--- src/main.js
+++ src/main.js
@@ -13,8 +13,13 @@ sumarParesFor(){
 contarImparesFor(inicio,fin){
     let impares=0;
 
-    for(let i=inicio; i<=fin ;i= i+1){
-        if(i%2!==0){
+    if(inicio>fin){
+        let t= inicio;
+        inicio=fin;
+        fin=t;
+    }
+    for(let i=inicio; i<=fin; i=i+1){
+        if(i%2!=0){
             impares= impares+1;
         }
     }
@@ -33,4 +38,4 @@ let app = new App();
 
 console.log(app.sumarParesFor());
 
-console.log(app.contarImparesFor(5,11));
+console.log(app.contarImparesFor(11,5));
