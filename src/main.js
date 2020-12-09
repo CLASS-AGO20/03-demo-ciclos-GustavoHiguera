ddc088c62aa6520f30555a66fa8abe43d37588c9
Gustavo Higuera <ghiguera@ucol.mx>
2020-12-09 12:12:09 -0600

    implemented contarImparesWhile()

--- src/main.js
+++ src/main.js
@@ -38,7 +38,17 @@ sumarParesWhile(){
     return suma;
 }
 
-
+contarImparesWhile(inicio,fin){
+    let i= inicio;
+    let impares= 0;
+    while(i<=fin){
+        if(i%2!==0){
+            impares++;
+        }
+        i++;
+    }
+    return impares;
+}
 
 
 
@@ -51,3 +61,5 @@ console.log(app.sumarParesFor());
 console.log(app.contarImparesFor(11,5));
 
 console.log(app.sumarParesWhile());
+
+console.log(app.contarImparesWhile(5,11));
