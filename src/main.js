b971aa20b51fec38dc47b4e4f9026afa06dc5bb2
Gustavo Higuera <ghiguera@ucol.mx>
2020-12-09 12:06:31 -0600

    implemented sumarParesWhile

--- src/main.js
+++ src/main.js
@@ -26,7 +26,17 @@ contarImparesFor(inicio,fin){
     return impares;
 }
 
-
+sumarParesWhile(){
+    let i=1;
+    let suma=0;
+    while(i<=20){
+        if(i%2===0){
+            suma=suma+i;
+        }
+        i=i+1;
+    }
+    return suma;
+}
 
 
 
@@ -39,3 +49,5 @@ let app = new App();
 console.log(app.sumarParesFor());
 
 console.log(app.contarImparesFor(11,5));
+
+console.log(app.sumarParesWhile());
